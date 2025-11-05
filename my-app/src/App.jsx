0794a479dfda0d382e91e94d4e27dedf0e9c3f43
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-11-05 03:45:47 -0800

    jsx-components

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-
-const App = () => {
+import About from "./About"
+import "./App.css"
+function App() {
+  const Name = "Abdullah"
   return (
     <div>
-      <h1>
-        hello ikram
+        <About  />
+        <h2 style={{backgroundColor:"yellowgreen"}}>
+          hello react js
+        </h2>
+      <h1 className="handing">
+        hello world shoaib {Name}
       </h1>
+      < About />
     </div>
   )
 }
 
 export default App
+
+
