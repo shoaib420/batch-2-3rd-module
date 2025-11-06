68d817380b09bfe10625a781b8697772b572221b
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-11-06 03:26:08 -0800

    props-conditional-rendering

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,25 @@
-import About from "./About"
+// import About from "./About"
+import Greet from './Component/Greet'
 import "./App.css"
 function App() {
-  const Name = "Abdullah"
+  // const Name = "Abdullah"
   return (
     <div>
-        <About  />
+      {/* <About  />
         <h2 style={{backgroundColor:"yellowgreen"}}>
           hello react js
         </h2>
       <h1 className="handing">
         hello world shoaib {Name}
       </h1>
-      < About />
+      < About /> */}
+
+      <Greet name="Shahid" isAdult age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
+      {/* <Greet name="ikram" isAdult={false} age={23} /> */}
+      <Greet name="muhammad " isAdult age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
+      <Greet name="ali" age={23}  location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
+      {/* <Greet name="wasif" age={23} /> */}
+      <Greet name="javeed" age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
     </div>
   )
 }
---
+++ my-app/src/Component/Greet.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+ const Greet = (props) => {
+    const {name, age , location} = props;
+    const {city, country, street, zipCode} = location;
+    console.log(props, "props")
+    return (
+        <React.Fragment>
+            <h1>
+                hello i am {name} my age {age}
+                 {/* {isAdult === true ? "Adult" : "your child"} */}
+           
+            </h1>
+            <p>
+                     {/* {JSON.stringify(location)} */}
+                   
+            </p>
+            {location &&
+            <ul>
+                <li>city: {city}</li>
+                 <li>country: {country}</li>
+                  <li>street: {street}</li>
+                   <li>zipCode: {zipCode}</li>
+            </ul>
+            }
+        </React.Fragment>
+    )
+}
+
+export default Greet
+
