6b7445a98b5151697b6a9c7242f07617aaa3f1c0
shoaib soomro <64595451+shoaib420@users.noreply.github.com>
2025-11-11 03:42:10 -0800

    form

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,11 +2,13 @@
 import Greet from './Component/Greet'
 import Count from './Component/Count'
 import "./App.css"
+import Form from './Component/Form'
 function App() {
   // const Name = "Abdullah"
   return (
     <div>
- <Count />
+      <Form />
+ {/* <Count /> */}
 
       {/* <About  />
         <h2 style={{backgroundColor:"yellowgreen"}}>
@@ -17,12 +19,13 @@ function App() {
       </h1>
       < About /> */}
 
-      <Greet name="Shahid" isAdult age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
+      {/* <Greet name="Shahid" isAdult age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} /> */}
       {/* <Greet name="ikram" isAdult={false} age={23} /> */}
-      <Greet name="muhammad " isAdult age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
-      <Greet name="ali" age={23}  location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
+      {/* <Greet name="muhammad " isAdult age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} /> */}
+      {/* <Greet name="ali" age={23}  location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} /> */}
       {/* <Greet name="wasif" age={23} /> */}
-      <Greet name="javeed" age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} />
+      {/* <Greet name="javeed" age={23} location={{ city: "ghotki", country: "pakistan", street: "23, aabc", zipCode: 1234 }} /> */}
+      
     </div>
   )
 }
---
+++ my-app/src/Component/Form.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useRef } from 'react';
+import { useState } from 'react'
+
+const Form = () => {
+    const [name , setName] = useState("");
+    const [email , setEmail] = useState("");
+   const refpassword  = useRef()
+//    const invalidForm = name && email  && password 
+
+const invalidForm = name  && email  && refpassword.current?.value 
+
+    const onSubmit =(event)=>{
+         event.preventDefault()
+         console.log("event", event,{password:refpassword.current?.value, name, email, invalidForm})
+    }
+
+    console.log("render")
+    return (
+        <div>
+            <form action="" onSubmit={onSubmit}>
+               <div>
+                 <label htmlFor="Name">Name</label>
+                <input type="text" required name="" id="" placeholder='Enter your name ' value={name}
+                onChange={(e)=>{
+                    console.log(e.target.value, "e.target.value  : ")
+                    setName(e.target.value)
+                }}
+                />
+               </div>
+               <div>
+                 <label htmlFor="email">email </label>
+                <input type="email" required name="" id="" placeholder='Enter your email ' value={email}
+                 onChange={(e)=>{
+                    console.log(e.target.value, "e.target.value  : ")
+                    setEmail(e.target.value)
+                }}
+                />
+               </div>
+                <div>
+                 <label htmlFor="password">password </label>
+                <input type="password" required name="" id="password" placeholder='Enter your password '
+                ref={refpassword}
+                
+                />
+               </div>
+               <button disabled={!invalidForm}>submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
